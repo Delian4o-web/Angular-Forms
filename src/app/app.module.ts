06ceecbe02a61279627ad1afc37653c3a9191c56
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-06 15:01:14 +0200

    Remove unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ContactComponent } from "./contact/contact.component";
 
 @NgModule({
-  declarations: [AppComponent, ContactComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
