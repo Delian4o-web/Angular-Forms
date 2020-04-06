52d5b5b09982b421d8d43d417a7e7c88a3ec374e
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-06 11:40:17 +0200

    Add Form Builder group and OnSubmit method

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
 import { Validators } from "@angular/forms";
 import { phoneNumberValidator } from "../forms/validators/phone-validator";
 @Component({
@@ -8,7 +8,32 @@ import { phoneNumberValidator } from "../forms/validators/phone-validator";
   styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
-  constructor() {}
+  registerForm: FormGroup;
+  submitted = false;
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      name: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      username: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+      confirmPassword: ["", [Validators.required]],
+    });
+  }
+
+  get registerFormControl() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      alert(
+        "Form Submitted succesfully!!!\n Check the values in browser console."
+      );
+      console.table(this.registerForm.value);
+    }
+  }
 }
