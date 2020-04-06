b3c4a0fdb74bc641ed43cd9d565d783935b94257
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-06 10:42:07 +0200

    Add Formgroup and formcontrol instance

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  profileForm = new FormGroup({
+    title: new FormControl(""),
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    phoneNo: new FormControl(""),
+    email: new FormControl(""),
+    gender: new FormControl(""),
+  });
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
