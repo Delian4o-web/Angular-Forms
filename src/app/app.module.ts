e321b0bb9f29f8ebfaf0bfd15a7dd9472925621c
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-06 15:14:52 +0200

    Add Contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AddressComponent } from './address/address.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, AddressComponent],
+  declarations: [AppComponent, AddressComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
