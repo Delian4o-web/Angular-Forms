5b340c579de7e33a42c571b6d11a921fd5bc9d55
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-06 13:53:39 +0200

    Add province array and onSubmit method

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-address',
-  templateUrl: './address.component.html',
-  styleUrls: ['./address.component.scss']
+  selector: "app-address",
+  templateUrl: "./address.component.html",
+  styleUrls: ["./address.component.scss"],
 })
 export class AddressComponent implements OnInit {
+  provinces = [
+    "Eastern Cape",
+    "Free State",
+    "Gauteng",
+    "KwaZulu-Natal",
+    "Limpopo",
+    "Mpumalanga",
+    "North West",
+    "Northern Cape",
+    "Western Cape",
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(form) {
+    console.log(form.value);
   }
-
 }
