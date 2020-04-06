e90ded7fd29908a041015736f1ece34e85988008
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-06 12:52:24 +0200

    Add pattern validator

--- src/app/services/customvalidation.service.ts
+++ src/app/services/customvalidation.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { ValidatorFn, AbstractControl } from "@angular/forms";
+import { FormGroup } from "@angular/forms";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CustomvalidationService {
-
-  constructor() { }
+  patternValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!control.value) {
+        return null;
+      }
+      const regex = new RegExp("/^+27/");
+      const valid = regex.test(control.value);
+      return valid ? null : { invalidPassword: true };
+    };
+  }
 }
