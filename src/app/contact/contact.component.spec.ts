22aa22988e622a84d5496819186929a752d61fdd
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-14 08:08:24 +0200

    Add unit test to test if onSubmit method is called

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,25 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ContactComponent } from './contact.component';
+import { ContactComponent } from "./contact.component";
+import { DebugElement } from "@angular/core";
+import { BrowserModule, By } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-describe('ContactComponent', () => {
+describe("ContactComponent", () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ContactComponent],
+      imports: [BrowserModule, FormsModule, ReactiveFormsModule],
     })
-    .compileComponents();
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(ContactComponent);
+
+        component = fixture.componentInstance;
+
+        de = fixture.debugElement.query(By.css("form"));
+        el = de.nativeElement;
+      });
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ContactComponent);
-    component = fixture.componentInstance;
+  it("should call the onSubmit method", () => {
     fixture.detectChanges();
-  });
+    spyOn(component, "onSubmit");
+    el = fixture.debugElement.query(By.css("button")).nativeElement;
+    el.click();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component.onSubmit).toHaveBeenCalledTimes(1);
   });
 });
