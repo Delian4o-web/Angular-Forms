0107dd7fe9b079af0f942c14d60fd8b776dc65d9
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-15 08:41:25 +0200

    Add neccessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AddressComponent } from "./address/address.component";
 import { FormsModule } from "@angular/forms";
+import { ContactComponent } from "./contact/contact.component";
 
 @NgModule({
-  declarations: [AppComponent, AddressComponent],
+  declarations: [
+    AppComponent,
+    AddressComponent,
+    ContactComponent,
+    ReactiveFormsModule,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
