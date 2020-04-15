d18abe526956ecf846ba335c2d2ef8d520e35a2a
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-15 09:14:07 +0200

    Import forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ContactComponent } from "./contact/contact.component";
+import { AddressComponent } from "./address/address.component";
 
 @NgModule({
-  declarations: [AppComponent, ContactComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  declarations: [AppComponent, ContactComponent, AddressComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
